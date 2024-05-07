27860cd05a9b70dea41c8e67f34b916358e0fb9b
mouhcinlz <154703960+mouhcinlz@users.noreply.github.com>
2024-05-07 06:58:12 -1200

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,11 @@
+<!-- Start footer-->
+<div class="footer">
+      <div class="container">
+         <div class="logo">
+            <img src="./images/Farmvetlogo_white.png" alt="">
+         </div>
+         <div class="copyright">
+            <p>&copy; 2024 Farmvet. All right reserved</p>
+         </div>
+      </div>
+   </div>
